2268ffdd2bfcdea1ed2e53a8dcffb2fbddcbfe9b
Ajay <Ajay>
2016-07-14 13:26:00 +0530

    new

--- app/Http/Controllers/Backend/Newsfeed/AdminNewsfeedController.php
+++ app/Http/Controllers/Backend/Newsfeed/AdminNewsfeedController.php
@@ -1,15 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Backend\Newsfeed;
-
-/*use App\Http\Requests\Backend\Admin\Newsfeed\NewsfeedRequests;*/
-/*use App\Models\Newsfeed\Newsfeed;*/
-/*use App\Repositories\Backend\Newsfeed\EloquentNewsfeedRepository;
-use App\Repositories\Backend\Logs\LogsActivitysRepository;*/
-use App\Models\Newsfeed\Newsfeed;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Activity;
+use App\Models\Newsfeed\Newsfeed;
 
 class AdminNewsfeedController extends Controller {
 
@@ -22,11 +15,7 @@ class AdminNewsfeedController extends Controller {
      * AdminCompanyController constructor.
      * @param EloquentCompanyRepository $companyRepository
      */
-   /* public function __construct(EloquentNewsfeedRepository $newsfeedRepository,LogsActivitysRepository $userlogs) {
-
-        $this->newsfeedRepository = $newsfeedRepository;
-        $this->userlogs = $userlogs;
-    }*/
+   
 
     /**
      * Display a listing of the resource.
