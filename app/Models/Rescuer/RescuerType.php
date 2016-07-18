4fab21cf660b22cb2cb61f437bcb70865a573d3f
Ajay <Ajay>
2016-07-18 18:55:48 +0530

    modification

--- app/Models/Rescuer/RescuerType.php
+++ app/Models/Rescuer/RescuerType.php
@@ -1,12 +1,8 @@
 <?php
 namespace App\Models\Rescuer;
 
-use App\Models\Access\User\Traits\UserAccess;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Models\Access\User\Traits\Attribute\UserAttribute;
-use App\Models\Access\User\Traits\Relationship\UserRelationship;
+
 
 /**
  * Class User
