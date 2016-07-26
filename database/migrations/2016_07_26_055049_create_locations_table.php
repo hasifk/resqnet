68e782969077e69aaec260c0f7d97ea12dafc2ae
Ajay <Ajay>
2016-07-26 11:28:08 +0530

    locations.

---
+++ database/migrations/2016_07_26_055049_create_locations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('lat');
+            $table->string('long');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropForeign('locations_user_id_foreign');
+        });
+        Schema::drop('locations');
+    }
+}
