6576a411eaeadda54c6a7949fb244186e436ba6c
ajayvayalil <ajayvayalilnext@gmail.com>
2016-08-19 13:29:22 +0530

    Doctors Updation.

--- database/migrations/2016_07_01_122919_create_doctors_table.php
+++ database/migrations/2016_07_01_122919_create_doctors_table.php
@@ -14,7 +14,7 @@ class CreateDoctorsTable extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->integer('user_id')->unsigned()->unique();
             $table->string('name');
             $table->string('surname');
             $table->string('phone');
