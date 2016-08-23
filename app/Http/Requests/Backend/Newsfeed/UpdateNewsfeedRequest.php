2fbe17d76b42a2f0fd8bff058f292d4059b9b21b
ajayvayalil <ajayvayalilnext@gmail.com>
2016-08-23 13:05:56 +0530

    UpdateNewsFeed

--- app/Http/Requests/Backend/Newsfeed/UpdateNewsfeedRequest.php
+++ app/Http/Requests/Backend/Newsfeed/UpdateNewsfeedRequest.php
@@ -3,7 +3,7 @@ namespace App\Http\Requests\Backend\Newsfeed;
 
 use Dingo\Api\Http\FormRequest;
 
-class CreateNewsfeedRequest extends FormRequest
+class UpdateNewsfeedRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,9 +25,7 @@ class CreateNewsfeedRequest extends FormRequest
         return [
             'news' =>'required',
             'news_title' =>'required',
-            'countryid' =>'required',
-            'newsfeed_type' =>'required',
-            'img'   =>'required|mimes:jpeg,bmp,png'
+            'id'=>'required'
         ];
     }
 }
