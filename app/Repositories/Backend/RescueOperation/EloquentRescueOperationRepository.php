dafd4ff6509c159ec0f95b3a61e52bb40b79daf9
hasifk <hasif.silverbloom@gmail.com>
2016-10-10 12:43:45 +0530

    request cancel

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -66,7 +66,7 @@ class EloquentRescueOperationRepository {
 
     public function rescueeOperationCancel($request) {
         $obj = ActiveRescuer::find($request->panicid);
-        $obj->status=0;
+        $obj->status = 0;
         $obj->save();
     }
 
@@ -185,29 +185,41 @@ class EloquentRescueOperationRepository {
     }
 
     public function rescuersResponse($request) {
-        $operation = $this->findOperation($request->active_rescuers_id);
-        if (empty($operation)):
-            $obj = new Operation;
-            $obj->active_rescuers_id = $request->active_rescuers_id;
-            $obj->rescuer_id = $request->rescuer_id;
-            $obj->save();
-            $rescuee_id = $this->ActiveRescuer($request->active_rescuers_id)->value('rescuee_id');
+        if (ActiveRescuer::where('id', $request->panicid)->value('status') == 1) {
+            $operation = $this->findOperation($request->active_rescuers_id);
+            if (empty($operation)):
+                $obj = new Operation;
+                $obj->active_rescuers_id = $request->active_rescuers_id;
+                $obj->rescuer_id = $request->rescuer_id;
+                $obj->save();
+                $rescuee_id = $this->ActiveRescuer($request->active_rescuers_id)->value('rescuee_id');
+                $user = User::find($request->rescuer_id);
+                $message['message'] = $user->firstname . " " . $user->lastname . " Accepted Your Request";
+                $message['id'] = $request->active_rescuers_id;
+                $message['to'] = "User";
+                $user = User::find($rescuee_id);
+            else:
+                $user = User::find($request->rescuer_id);
+                $message['message'] = "Another Rescuer Accepted this request";
+                $message['id'] = $request->active_rescuers_id;
+                $message['to'] = "Rescuer";
+            endif;
+            $app_id['app_id'][] = $user->app_id;
+            $app_id['device_type'][] = $user->device_type;
+
+            $this->notification($app_id, $message);
+            return $request->active_rescuers_id;
+        }
+        else {
             $user = User::find($request->rescuer_id);
-            $message['message'] = $user->firstname . " " . $user->lastname . " Accepted Your Request";
-            $message['id'] = $request->active_rescuers_id;
-            $message['to'] = "User";
-            $user = User::find($rescuee_id);
-        else:
-            $user = User::find($request->rescuer_id);
-            $message['message'] = "Another Rescuer Accepted this request";
+            $message['message'] = "This Request has been cancelled by the user";
             $message['id'] = $request->active_rescuers_id;
             $message['to'] = "Rescuer";
-        endif;
-        $app_id['app_id'][] = $user->app_id;
-        $app_id['device_type'][] = $user->device_type;
-
-        $this->notification($app_id, $message);
-        return $request->active_rescuers_id;
+            $app_id['app_id'][] = $user->app_id;
+            $app_id['device_type'][] = $user->device_type;
+            $this->notification($app_id, $message);
+            return $request->active_rescuers_id;
+        }
     }
 
     public function distanceCalculation($point1_lat, $point1_long, $point2_lat, $point2_long, $unit = 'km', $decimals = 2) {
