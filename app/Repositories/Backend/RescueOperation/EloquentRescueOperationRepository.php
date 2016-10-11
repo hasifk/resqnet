0a9110deec3e2d9c58d7af9fbdb47034a64456f6
hasifk <hasif.silverbloom@gmail.com>
2016-10-11 16:18:19 +0530

    resqnet panic

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -102,11 +102,11 @@ class EloquentRescueOperationRepository {
                     'Content-Type: application/json'
                 );
                 $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, 'https://gcm-http.googleapis.com/gcm/send');
+                curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
                 curl_setopt($ch, CURLOPT_POST, true);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
                 $result = curl_exec($ch);
 //echo $result;
