cd86d1d47eadf9ad5d834682d918752f8e70c874
hasifk <hasif.silverbloom@gmail.com>
2016-10-11 15:38:40 +0530

    api keys

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -49,14 +49,14 @@ class EloquentRescueOperationRepository {
             $message['id'] = $obj->id;
             if (!empty($rescuers)) {
                 $message['to'] = "Rescuer";
-                return $this->notification($app_id, $message);
+                $this->notification($app_id, $message);
                 $userdetails['result'] = 'SUCCESS';
                 $userdetails['panicid'] = $obj->id;
             } else
                 $userdetails['result'] = "No Rescuers available";
             if (!empty($appids)) {
                 $message['to'] = "Emergency";
-               return $this->notification($appids[0], $message);
+                $this->notification($appids[0], $message);
             }
         } else
             $userdetails['result'] = "Please enable Location services";
@@ -90,7 +90,7 @@ class EloquentRescueOperationRepository {
                     'panicid' => $message['id'],
                     'notification_type' => $message['to']
                 );
-                $fields[] = array
+                $fields = array
                     (
                     'registration_ids' => array($app_id['app_id'][$key]),
                     'data' => $msg
@@ -116,12 +116,12 @@ class EloquentRescueOperationRepository {
                
                 // Close connection
                 curl_close($ch);
-                //echo $result;
+                echo $result;
             } else {
                 
             }
         }
-        return $fields;
+       // return $fields;
     }
 
     public function emergencyContacts($id) {
