8c635186663827926d0e8a0744bf493085926697
hasifk <hasif.silverbloom@gmail.com>
2016-10-12 10:21:10 +0530

    api key updated

--- app/Repositories/Backend/Notifications/EloquentNotificationRepository.php
+++ app/Repositories/Backend/Notifications/EloquentNotificationRepository.php
@@ -97,7 +97,7 @@ class EloquentNotificationRepository implements NotificationRepositoryContract {
                 );
                 $headers = array
                     (
-                    'Authorization: key=' . 'AIzaSyAJ3hG9ryU-_7mrdZYDOHBkTUWP8TyUoCA',
+                    'Authorization: key=AIzaSyAJ3hG9ryU-_7mrdZYDOHBkTUWP8TyUoCA',
                     'Content-Type: application/json'
                 );
                 $ch = curl_init();
