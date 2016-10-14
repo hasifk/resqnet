3abf94ac80268db2358290c46e27525c4df42546
ajayvayalil <ajayvayalilnext@gmail.com>
2016-10-14 12:54:06 +0530

    Migrations Update

--- database/migrations/2016_10_14_055003_user_group_table.php
+++ database/migrations/2016_10_14_055003_user_group_table.php
@@ -14,7 +14,7 @@ class UserGroupTable extends Migration
     {
         Schema::create('user_group', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->unique();
+            $table->integer('user_id')->unsigned();
             $table->string('name');
             $table->string('gp_image_filename');
             $table->string('gp_image_extension');
