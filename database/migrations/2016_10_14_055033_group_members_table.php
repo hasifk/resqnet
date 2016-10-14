8c913906a4b04e50a57613ac122369820ac84b94
ajayvayalil <ajayvayalilnext@gmail.com>
2016-10-14 12:00:41 +0530

    new migrations

---
+++ database/migrations/2016_10_14_055033_group_members_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GroupMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->unique();
+            $table->integer('group_id')->unsigned()->unique();
+            $table->boolean('role')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('user_group')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('group_members', function (Blueprint $table) {
+            $table->dropForeign('group_members_user_id_foreign');
+            $table->dropForeign('group_members_group_id_foreign');
+        });
+        Schema::drop('user_group');
+    }
+}
