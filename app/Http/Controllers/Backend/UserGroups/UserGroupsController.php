2b7362a4e9ef2aa3ee162a237b5b7d03cf23642a
hasifk <hasif.silverbloom@gmail.com>
2016-10-14 12:27:04 +0530

    access service provider

--- app/Http/Controllers/Backend/UserGroups/UserGroupsController.php
+++ app/Http/Controllers/Backend/UserGroups/UserGroupsController.php
@@ -9,35 +9,35 @@ class UserGroupsController extends Controller {
 
     private $operation;
 
-    public function __construct(AdminOperationRepositoryContract $operation) {
+    public function __construct(UserGroupsRepositoryContract $operation) {
 
         $this->operation = $operation;
     }
 
     public function userGroups() {
         $view = [
-            'operations' => $this->operation->getOperations(),
+            'operations' => $this->operation->userGroups($request),
         ];
         return view('backend.operations.index', $view);
     }
 
     public function CreateUserGroups() {
         $view = [
-            'operations' => $this->operation->getOperations(),
+            'operations' => $this->operation->CreateUserGroups(),
         ];
         return view('backend.operations.index', $view);
     }
 
     public function setAdministrator() {
         $view = [
-            'operations' => $this->operation->getOperations(),
+            'operations' => $this->operation->setAdministrator(),
         ];
         return view('backend.operations.index', $view);
     }
 
     public function postNewsFeed() {
         $view = [
-            'operations' => $this->operation->getOperations(),
+            'operations' => $this->operation->postNewsFeed($request),
         ];
         return view('backend.operations.index', $view);
     }
