937d8cc9e862440ce7b07f240b3933874e2f0b1c
hasifk <hasif.silverbloom@gmail.com>
2016-10-25 13:24:22 +0530

    paypal

--- app/Http/Controllers/Backend/UserGroups/UserGroupsController.php
+++ app/Http/Controllers/Backend/UserGroups/UserGroupsController.php
@@ -42,7 +42,7 @@ class UserGroupsController extends Controller {
     }
 
     public function joinedGroupLists(Request $request) {
-         if(count($lists=$this->groups->joinedGroupLists($request))>0)
+         if(count($lists=$this->groups->joinedGroupLists($request->user_id))>0)
          {
               return response()->json(['operation' => $lists]);
          }
@@ -75,7 +75,7 @@ class UserGroupsController extends Controller {
     }
     public function payPal(Request $request) {
         
-         Mail::send('frontend.auth.emails.paypal', ['token' => $request], function ($message) use ($request) {
+         Mail::send('frontend.auth.emails.paypal', ['token' => "hello"], function ($message) use ($request) {
                     $message->to("edwinmathew63@gmail.com");
                 });
         //mail("edwinmathew63@gmail.com","My subject",$request);
