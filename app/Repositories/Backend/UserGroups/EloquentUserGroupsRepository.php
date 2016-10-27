743ff8950f27fd6f69238904b4ec92a67dcf4454
hasifk <hasif.silverbloom@gmail.com>
2016-10-27 15:53:39 +0530

    gp_pin

--- app/Repositories/Backend/UserGroups/EloquentUserGroupsRepository.php
+++ app/Repositories/Backend/UserGroups/EloquentUserGroupsRepository.php
@@ -201,7 +201,7 @@ class EloquentUserGroupsRepository implements UserGroupsRepositoryContract {
             $return = "Error...gp_pin not found";
 
         if (count($request->gp_pin) == $f)
-            $return = 1;
+            $return = "00";
         else
             return $return;
     }
