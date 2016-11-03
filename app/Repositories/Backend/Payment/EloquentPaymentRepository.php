7ee1d08e6a61c157d52b3eea401307f2147266a3
hasifk <hasif.silverbloom@gmail.com>
2016-11-03 16:59:45 +0530

    payment

--- app/Repositories/Backend/Payment/EloquentPaymentRepository.php
+++ app/Repositories/Backend/Payment/EloquentPaymentRepository.php
@@ -11,21 +11,21 @@ class EloquentPaymentRepository implements PaymentRepositoryContract {
 
     public function paymentSave($request) {
         $dt = Carbon::now();
-      
-        
+        $date = $dt->addYears(1);
+
         $obj = new Payment;
         $obj->user_id = $request->custom;
         $obj->txn_id = $request->txn_id;
         $obj->ipn_track_id = $request->ipn_track_id;
         $obj->payment_status = $request->payment_status . " " . $request->payment_date;
-        $obj->subscription_ends_at = date('Y-M-d',strtotime($dt->addYears(1)));
+        $obj->subscription_ends_at = date('Y-m-d', strtotime($lastdate));
         // $obj->payment_date = $request->payment_date;
         $obj->save();
         //return $obj;
     }
 
     public function paymentDetails($request) {
-        return Payment::where('user_id',$request->user_id)->orderBy('id', 'desc')->first();
+        return Payment::where('user_id', $request->user_id)->orderBy('id', 'desc')->first();
     }
 
 }
