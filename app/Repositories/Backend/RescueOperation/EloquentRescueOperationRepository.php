caaa4fe7549df9b733e625a7862fd422ab6379d1
hasifk <hasif.silverbloom@gmail.com>
2016-11-04 16:21:14 +0530

    rescuerlists

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -354,18 +354,18 @@ class EloquentRescueOperationRepository {
                 $res3[] = $this->groups->userGroupdetails($k, $gp_user_id);
             endif;
             $rescuers['emergency_groups'] = $res3;
-            $operation = Operation::where('active_rescuers_id', $rescuers->id)->first();
+            $operation = Operation::where('active_rescuers_id', $active->id)->first();
             if (!empty($operation)) {
-                $rescuers['tagged'] = User::find($operation->rescuer_id);
-                $activetime = strtotime($rescuers->created_at);
+                $rescuers[$key]['tagged'] = User::find($operation->rescuer_id);
+                $activetime = strtotime($active->created_at);
                 $operationtime = strtotime($operation->created_at);
                 if (!empty($operation->finished_at)):
                     $finishedtime = strtotime($operation->finished_at);
                     $tot_sec = round(abs($finishedtime - $operationtime));
-                    $rescuers['rescuerresponse'] = $this->timeCalculator($tot_sec);
+                    $rescuers[$key]['finished'] = $this->timeCalculator($tot_sec);
                 endif;
                 $tot_sec = round(abs($operationtime - $activetime));
-                $rescuers['panicresponse'] = $this->timeCalculator($tot_sec);
+                $rescuers[$key]['rescuerresponse'] = $this->timeCalculator($tot_sec);
             }
         }
         return $rescuers;
