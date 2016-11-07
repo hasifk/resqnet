0689141277798723a77b75ec6cc273b2fc0a7b4c
hasifk <hasif.silverbloom@gmail.com>
2016-11-07 12:43:37 +0530

    ios push

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -170,7 +170,7 @@ class EloquentRescueOperationRepository {
             } else {
 // Provide the Host Information.
                 //$tHost = 'gateway.sandbox.push.apple.com';
-                $tHost = gateway.push.apple.com;
+                $tHost = 'gateway.push.apple.com';
 //$tHost = 'gateway.push.apple.com';
                 $tPort = 2195;
 // Provide the Certificate and Key Data.
