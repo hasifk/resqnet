a5f42ee1f105cb547f71192e9af8f2e64b32d2e2
hasifk <hasif.silverbloom@gmail.com>
2016-11-07 12:57:32 +0530

    push ios

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -181,8 +181,8 @@ class EloquentRescueOperationRepository {
                 $tPassphrase = 'SilverBloom1978';
 // Provide the Device Identifier (Ensure that the Identifier does not have spaces in it).
 // Replace this token with the token of the iOS device that is to receive the notification.
-//$tToken = 'b3d7a96d5bfc73f96d5bfc73f96d5bfc73f7a06c3b0101296d5bfc73f38311b4';
-                $tToken = array($app_id['app_id'][$key]);
+$tToken = 'a18792a07ae2c4caf346332e4fbe5ba8071d5b6d66ef6cd3731f6c78ecdc258a';
+               // $tToken = $app_id['app_id'][$key];
 //0a32cbcc8464ec05ac3389429813119b6febca1cd567939b2f54892cd1dcb134
 // The message that is to appear on the dialog.
                 $tAlert = $message['message'];
