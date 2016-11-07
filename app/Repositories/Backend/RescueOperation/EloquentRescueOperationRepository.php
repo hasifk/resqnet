63a1de9861781f711b3bb9e62427d3ca6f6e0664
hasifk <hasif.silverbloom@gmail.com>
2016-11-07 16:48:32 +0530

    ios push

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -100,18 +100,18 @@ class EloquentRescueOperationRepository {
             $message['id'] = $obj->id;
             if (!empty($rescuers)) {
                 $message['to'] = "Rescuer";
-                $this->notification($app_id, $message);
+                $userdetails[] =$this->notification($app_id, $message);
                 $userdetails['result'] = 'SUCCESS';
                 $userdetails['panicid'] = $obj->id;
             } else
                 $userdetails['result'] = "No Rescuers available";
             if (!empty($appids)) {
                 $message['to'] = "Emergency";
-                $this->notification($appids[0], $message);
+               $userdetails[] = $this->notification($appids[0], $message);
             }
             if (!empty($groups)) {
                 $message['to'] = "EmergencyGroup";
-                $this->notification($groups[0], $message);
+               $userdetails[] = $this->notification($groups[0], $message);
             }
         } else
             $userdetails['result'] = "Please enable Location services";
@@ -223,7 +223,7 @@ class EloquentRescueOperationRepository {
                 $tResult = fwrite($tSocket, $tMsg, strlen($tMsg));
 
                 $tResult = fwrite($tSocket, $tMsg);
-               // return $tResult;
+                return $tResult;
 //                if ($tResult)
 //                    return 'Delivered Message to APNS';
 //                else
