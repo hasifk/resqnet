9618349a78757b1b13d556366c55104aa1a45919
hasifk <hasif.silverbloom@gmail.com>
2016-11-14 14:30:38 +0530

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,12 +32,6 @@ class AuthController extends Controller {
     public function postLogin(Request $request) {
         if (\Auth::attempt(['email' => $request->get('email'), 'password' => $request->get('password')])) {
             $user = \Auth::user();
-            if (!empty($users = User::where('app_id',$request->app_id)->get()))
-            {
-                $users->app_id="";
-                $users->save;
-            }
-            
             $user->app_id = $request->app_id;
             $user->device_type = $request->device_type;
             $user->save();
