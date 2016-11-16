19978a5a713ee0762e3e4922188bb7631f18fac5
hasifk <hasif.silverbloom@gmail.com>
2016-11-16 14:38:58 +0530

    ios notif

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -221,7 +221,7 @@ class EloquentRescueOperationRepository {
                 
                 $tHost = 'gateway.sandbox.push.apple.com';
 				$tPort = 2195;
-				$tCert = realpath('public/') . 'pushcert.pem';
+				$tCert = base_path('public/') . 'pushcert.pem';
 				return $tCert;
 				$tPassphrase = 'SilverBloom1978';
 				$tAlert = 'Match scheduled at ';
