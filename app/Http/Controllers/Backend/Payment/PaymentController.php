3b03624e884c093e9efa12d5073c5c08bf60afb9
hasifk <hasif.silverbloom@gmail.com>
2016-12-05 13:49:26 +0530

    payment

--- app/Http/Controllers/Backend/Payment/PaymentController.php
+++ app/Http/Controllers/Backend/Payment/PaymentController.php
@@ -22,19 +22,18 @@ class PaymentController extends Controller {
     }
 
     public function paymentDetails(Request $request) {
-        $res['user_status']='';
-        $res['email_confirmed']='';
-         if (!empty($user=User::withTrashed()->where('id',$request->user_id)->first())){
-              $res['user_status']=$user->status;
-              $res['email_confirmed']=$user->confirmed;
-          }
-        if (!empty($result = $this->payment->paymentDetails($request))){
-            $result[]=$res;
-            return response()->json(['result' => $result]);
+        $user_status = '';
+        $email_confirmed = '';
+        if (!empty($user = User::withTrashed()->where('id', $request->user_id)->first())) {
+            $user_status = $user->status;
+            $email_confirmed = $user->confirmed;
         }
-        else
+        if (!empty($result = $this->payment->paymentDetails($request))) {
+            $result['user_status'] = $user_status;
+            $result['email_confirmed'] = $email_confirmed;
+            return response()->json(['result' => $result]);
+        } else
             return response()->json(['result' => 'No payment details Yet']);
-        
     }
 
     public function payeeDetails() {
