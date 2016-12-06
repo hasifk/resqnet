3b78cc8e0e8dfb38db1c0824b9990420948cc6a9
ajayvayalil <ajayvayalilnext@gmail.com>
2016-12-06 15:48:58 +0530

    Payment Details Update

--- app/Repositories/Backend/Payment/EloquentPaymentRepository.php
+++ app/Repositories/Backend/Payment/EloquentPaymentRepository.php
@@ -11,6 +11,8 @@ use Carbon\Carbon;
 class EloquentPaymentRepository implements PaymentRepositoryContract {
 
     public function paymentSave($request) {
+        $check_payment=Payment::where('txn_id',$request->txn_id)->first();
+        if(empty($check_payment)):
         $dt = Carbon::now();
         $obj = new Payment;
         $obj->user_id = $request->custom;
@@ -19,10 +21,16 @@ class EloquentPaymentRepository implements PaymentRepositoryContract {
         $obj->payment_status = $request->payment_status;
         $obj->subscription_ends_at = $dt->addYears(1);
         $obj->save();
+        else:
+            $check_payment->ipn_track_id = $request->ipn_track_id;
+            $check_payment->payment_status = $request->payment_status;
+            $check_payment->save();
+            endif;
+
     }
 
     public function paymentDetails($request) {
-        return Payment::where('user_id', $request->user_id)->orderBy('id', 'desc')->first();
+        return Payment::where('user_id', $request->user_id)->whereIn('payment_status', ['Completed','Created','Processed'])->orderBy('id', 'desc')->first();
     }
     public function paymentChecking($id) {
         return Payment::where('user_id',$id)->orderBy('id', 'desc')->first();
