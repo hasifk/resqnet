22e24d6922867b0726288599c77890ac2687d97b
ajayvayalil <ajayvayalilnext@gmail.com>
2017-01-10 17:49:04 +0530

    test

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -244,11 +244,23 @@ class EloquentRescueOperationRepository {
                 'Authorization: key=' . API_ACCESS_KEY,
                 'Content-Type: application/json'
             );
-           
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+            $result = curl_exec($ch);
+            //$code = curl_getinfo ($ch, CURLINFO_HTTP_CODE);
+            //return $code;
+// Close connection
+            curl_close($ch);
+
 
         }
 
-        else {
+       if (!empty($ios_ids)&&count($ios_ids) > 0) {
 
             $tHost = 'gateway.push.apple.com';
             $tPort = 2195;
