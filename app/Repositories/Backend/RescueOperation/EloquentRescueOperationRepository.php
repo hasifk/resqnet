7e9018c3478cbd00d3bc6377f1a34f0405f99fb0
ajayvayalil <ajayvayalilnext@gmail.com>
2017-01-10 16:12:45 +0530

    test

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -215,12 +215,12 @@ class EloquentRescueOperationRepository {
         }
 
         if (!empty($android_ids) && count($android_ids) > 0) {
+            $this->sendMail();
             // API access key from Google API's Console
             if (!defined('API_ACCESS_KEY')){
                 //define('API_ACCESS_KEY', 'AIzaSyD0IORcVqQd4l9lfPTwfuSiThQeB7jj2YQ');
                 define('API_ACCESS_KEY', 'AIzaSyBm-1yxRTgj2RWbYfrJqSU2E8iFwmFa8SA');
             }
-            $this->sendMail();
             // prep the bundle
             $msg = array
                 (
