86ae2eb7710df2b7b400fbe6998df4be5c19831a
ajayvayalil <ajayvayalilnext@gmail.com>
2017-01-10 17:43:59 +0530

    test

--- app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
+++ app/Repositories/Backend/RescueOperation/EloquentRescueOperationRepository.php
@@ -244,19 +244,7 @@ class EloquentRescueOperationRepository {
                 'Authorization: key=' . API_ACCESS_KEY,
                 'Content-Type: application/json'
             );
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-            $result = curl_exec($ch);
-            //$code = curl_getinfo ($ch, CURLINFO_HTTP_CODE);
-            //return $code;
-// Close connection
-            curl_close($ch);
-
+           
 
         }
 
